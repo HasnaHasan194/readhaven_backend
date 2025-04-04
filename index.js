1fcdc5506ea20e9d3d03b02072ef834a405d19f3
HasnaHasan194 <hasnahasnahasan194@gmail.com>
2025-04-04 20:20:38 +0530

    redeploy v_1

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ import { STATUS_CODES } from './utils/constants.js';
 dotenv.config()
 
 const app=express();
-app.use(express.json());
+app.use(express.json({ limit: "50mb" }));
 app.use(cookieParser());
 
 
